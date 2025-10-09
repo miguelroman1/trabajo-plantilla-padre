6f9a249d36d8d44905889fd561b9e3109be9b7de
miguelroman1 <23308060610314@cetis61.edu.mx>
2025-10-09 17:05:57 -0600

    avance

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 @app.route("/")
 @app.route("/inicio")
 def inicio():
-    """Ruta para l pagina de inicio"""
-    info = "Bienvenido a nuestra pagina. Aqui encontraras informacion sobre animales exoticos, vehiculos antiguos, maravillas del mundo y mas. !Explora el mundo superior para empezar¡"
-    return render_template("inicio.html", title ="inicio",info=info)
+    """Ruta para la página de inicio"""
+    info = "Bienvenido a nuestra página. Aquí encontrarás información sobre animales exóticos, vehículos antiguos, maravillas del mundo y más. ¡Explora el menú superior para empezar!"
+    return render_template("inicio.html", title="Inicio", info=info)
 
-@app.route("/animales-exoticos")
-def animales_exoticos():
-    """Ruta para la seccion de animales exoticos"""
-    contenido = "Esta seccion esta dedicada  a las especies mas raras y facsinantes del planeta. Conoce sus habitats, caracteristicas y esfuerzos de conservacion"
-    return render_template("animales.html", title = "Animales Exoticos", content = contenido)
+@app.route("/animales")
+def animales():
+    """Ruta para la sección de animales exóticos"""
+    contenido = "Esta sección está dedicada a las especies más raras y fascinantes del planeta. Conoce sus hábitats, características y esfuerzos de conservación."
+    return render_template("animales.html", title="Animales Exóticos", content=contenido)
     
-@app.route("/vehiculos-antiguos")
-def vehiculos_antiguos():
-    """Ruta para la seccion de vehiculos antiguos"""
-    contenido = "Un viaje al paado sobre ruedas. Explora la historia de los automoviles clasicos, su diseño atemporal y las leyendas detras de ellos"
-    return render_template("vehiculos.html", title = "Vehiculos Antiguos", content = contenido)
+@app.route("/vehiculos")
+def vehiculos():
+    """Ruta para la sección de vehículos antiguos"""
+    contenido = "Un viaje al pasado sobre ruedas. Explora la historia de los automóviles clásicos, su diseño atemporal y las leyendas detrás de ellos."
+    return render_template("vehiculos.html", title="Vehículos Antiguos", content=contenido)
 
-    
-    
-    
-    
+@app.route("/maravillas")
+def maravillas():
+    """Ruta para la sección de vehículos antiguos"""
+    contenido = "Un viaje al pasado sobre ruedas. Explora la historia de los automóviles clásicos, su diseño atemporal y las leyendas detrás de ellos."
+    return render_template("maravillas.html", title="Vehículos Antiguos", content=contenido)
+
+@app.route("/acerca")
+def acerca():
+    """Ruta para la sección de vehículos antiguos"""
+    contenido = "Un viaje al pasado sobre ruedas. Explora la historia de los automóviles clásicos, su diseño atemporal y las leyendas detrás de ellos."
+    return render_template("acerca.html", title="Vehículos Antiguos", content=contenido)
+
 if __name__ == '__main__':
     app.run(debug=True)
